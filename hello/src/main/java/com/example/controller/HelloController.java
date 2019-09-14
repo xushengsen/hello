c306f29818a3cce84a8da5a210f7756ec5fe6e00
xushengsen <1045125323@qq.com>
2019-09-14 11:47:08 +0800

    第一次提交

---
+++ hello/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**  
+ * ClassName:HelloController <br/>  
+ * Function:  <br/>  
+ * Date:     2019年8月18日 下午11:55:56 <br/>       
+ */
+@RestController
+@RequestMapping("/ni")
+public class HelloController {
+     @GetMapping("vi/hello")
+     public String hello() {
+         //1111
+            return "Hello World!";
+        }
+}
+  
